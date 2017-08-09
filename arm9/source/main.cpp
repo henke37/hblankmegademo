001571dca87350e97a23ff1586dbac02557d9b23
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2017-08-09 07:25:03 +0200

    Fix filesystem access

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -1,9 +1,14 @@
 #include <nds.h>
+#include <filesystem.h>
 #include "demo.h"
 #include "demoRunner.h"
 #include "menuDemo.h"
 
 int main(void) {
+
+	char **basePath=nullptr;
+
+	nitroFSInit(basePath);
 
 	runner=std::make_shared<MenuDemo>();
 	runner.start();
