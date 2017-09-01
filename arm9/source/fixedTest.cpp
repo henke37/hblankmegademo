a93646c17ddd494ca5b16011645490fcc7c4f4c8
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2017-09-01 14:22:22 +0200

    Mod needs to prescale the numerator

--- arm9/source/fixedTest.cpp
+++ arm9/source/fixedTest.cpp
@@ -2,8 +2,16 @@
 #include <cassert>
 
 void testFixed() {
-	fp8 a=5,b=4,c=3,d=1;
-	fp12 p,q,r,s;
+	fp8 a=5,b=4,c=3,d=1,e=2;
+	fp12 p=5,q=4,r=3,s=1,t=2;
 
+	assert(((int)d) == 1);
 	assert((a - b) == 1);
+	assert((b * c) == 12);
+	assert(a - c == e);
+	assert(a > b);
+	assert(b < a);
+	assert(sqrt(q) == 2);
+	assert((q / t) == 2);
+	assert((fp12)(p << 2) == 10);
 }
