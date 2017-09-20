2f40e9af0e100a056943356e145288396b8e3097
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2017-09-20 09:02:52 +0200

    Sinxscroll moved to fixed point math

--- arm9/source/sinXScrollDemo.cpp
+++ arm9/source/sinXScrollDemo.cpp
@@ -2,16 +2,16 @@
 #include "vrambatcher.h"
 #include <cmath>
 #include <nds/arm9/input.h>
+#include <nds/arm9/background.h>
 
-#define MIN_AMPLITUDE 0
-#define MAX_AMPLITUDE 256
-#define MAX_SPEED 0.5
-#define MIN_SPEED 0
-#define MAX_LINESPEED 0.6
-#define MIN_LINESPEED 0
-#define M_PI       3.14159265358979323846
+#define MIN_AMPLITUDE 0_fp12
+#define MAX_AMPLITUDE 256_fp12
+#define MAX_SPEED 5000_fixedAngle
+#define MIN_SPEED 0_fixedAngle
+#define MAX_LINESPEED 5000_fixedAngle
+#define MIN_LINESPEED 0_fixedAngle
 
-SinXScrollDemo::SinXScrollDemo() : offset(0), speed(0.04), amplitude(15), lineSpeed(0.01) {}
+SinXScrollDemo::SinXScrollDemo() : offset(0), speed(200), amplitude(15), lineSpeed(60) {}
 SinXScrollDemo::~SinXScrollDemo() {}
 
 void SinXScrollDemo::AcceptInput() {
@@ -23,15 +23,15 @@ void SinXScrollDemo::AcceptInput() {
 	}
 
 	if(keys & KEY_UP && speed < MAX_SPEED) {
-		speed += 0.01;
+		speed += 10;
 	} else if(keys & KEY_DOWN && speed > MIN_SPEED) {
-		speed -= 0.01;
+		speed -= 10;
 	}
 
 	if(keys & KEY_LEFT && lineSpeed > MIN_LINESPEED) {
-		lineSpeed -= 0.0025;
+		lineSpeed -= 5;
 	} else if(keys & KEY_RIGHT && lineSpeed < MAX_LINESPEED) {
-		lineSpeed += 0.0025;
+		lineSpeed += 5;
 	}
 }
 
@@ -42,14 +42,12 @@ void SinXScrollDemo::Load() {
 void SinXScrollDemo::Unload() {}
 void SinXScrollDemo::PrepareFrame(VramBatcher &batcher) {
 	offset += speed;
-	if(offset >= M_PI*2) offset -= M_PI*2;
 
-	float lineOffset = offset;
+	FixedAngle lineOffset = offset;
 	for (int scanline = 0; scanline < SCREEN_HEIGHT; ++scanline) {
-		int xscroll = std::sin(lineOffset)*amplitude;
-		batcher.AddPoke(scanline, xscroll, &REG_BG0HOFS);
+		fp12 xscroll = sin(lineOffset)*amplitude;
+		batcher.AddPoke(scanline, (int16_t)(int)xscroll, &REG_BG0HOFS);
 
 		lineOffset += lineSpeed;
-		//if(lineOffset >= M_PI * 2) lineOffset -= M_PI * 2;
 	}
 }
