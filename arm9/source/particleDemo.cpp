ddb25244fe439094ca3ae7825d858c115e295bcb
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2017-10-16 11:44:43 +0200

    Give the BaseParticleDemo class something fun to do
    Specifically, converting particles to objects

--- arm9/source/particleDemo.cpp
+++ arm9/source/particleDemo.cpp
@@ -19,16 +19,52 @@ void BaseParticleDemo::Unload() {
 	mainObjManager.deactivate();
 }
 
+void BaseParticleDemo::buildObjectsFromParticles() {
+
+	mainObjManager.objects.clear();
+	for(const Particle &particle : particles) {
+		SpriteEntry obj;
+
+		auto size = spriteSizeToEnum(particle.width, particle.height);
+
+		obj.isHidden = 0;
+		obj.isMosaic = 0;
+		obj.isRotateScale = 0;
+
+		obj.hFlip = 0;
+		obj.vFlip = 0;
+
+		obj.gfxIndex = particle.baseTile;
+		obj.palette = particle.palette;
+		obj.colorMode = OBJCOLOR_16;
+		obj.blendMode = OBJMODE_NORMAL;
+
+		obj.priority = OBJPRIORITY_3;
+
+		obj.x = particle.x;
+		obj.y = particle.y;
+
+		obj.size = (ObjSize)SPRITE_SIZE_SIZE(size);
+		obj.shape = (ObjShape) SPRITE_SIZE_SHAPE(size);
+
+		mainObjManager.objects.push_back(std::move(
+			obj
+		));
+	}
+}
+
 BaseParticleDemo::Particle::Particle() {}
-BaseParticleDemo::Particle::Particle(int x_, int y_) : x(x_), y(y_), width(1), height(1) {}
-BaseParticleDemo::Particle::Particle(int x_, int y_, int w, int h) : x(x_), y(y_), width(w), height(h) {}
+BaseParticleDemo::Particle::Particle(int x_, int y_, int baseTile_, int p_) : 
+	x(x_), y(y_), width(1), height(1), baseTile(baseTile_), palette(p_) {}
+BaseParticleDemo::Particle::Particle(int x_, int y_, int w, int h, int baseTile_, int p_) : 
+	x(x_), y(y_), width(w), height(h), baseTile(baseTile_), palette(p_) {}
 
 SpriteSize spriteSizeToEnum(int width, int height) {
 	//round the width and height upwards to a multipiel of 8
 	width +=7;//offset
 	height +=7;
 	width &= ~7;//and then round down
-	width &= ~7;
+	width &= ~7;//which turns out to be rounding up thans to the offset
 
 	if(width == 8) {
 		if(height == 8) return SpriteSize_8x8;
@@ -50,5 +86,5 @@ SpriteSize spriteSizeToEnum(int width, int height) {
 		if(height == 32) return SpriteSize_64x32;
 		if(height == 64) return SpriteSize_64x64;
 	}
-	sassert(0,"Bad object size found");
+	assert(0);
 }
